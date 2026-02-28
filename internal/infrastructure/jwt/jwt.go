38a11b5c9666c7b87eb5a55a9e3101ccc6a22c3f
alikurb12 <gurbansahedovali@gmail.com>
2026-02-28 15:18:09 +0300

    Implement token generation and validation service

--- internal/infrastructure/jwt/jwt.go
+++ internal/infrastructure/jwt/jwt.go
@@ -1,1 +1,111 @@
 package jwt
+
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/alikurb12/auth_service_jwt_golang/internal/domain"
+	"github.com/alikurb12/auth_service_jwt_golang/pkg/config"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+)
+
+type Claims struct {
+	UserID string      `json:"id"`
+	Role   domain.Role `json:"role"`
+	jwt.RegisteredClaims
+}
+
+type JWTService struct {
+	accessSecret  []byte
+	refreshSecret []byte
+	accessTTL     time.Duration
+	refreshTTL    time.Duration
+}
+
+func NewJWTService(cfg config.JWTConfig) *JWTService {
+	return &JWTService{
+		accessSecret:  []byte(cfg.AccessSecret),
+		refreshSecret: []byte(cfg.RefresjSecret),
+		accessTTL:     cfg.AccessTTL,
+		refreshTTL:    cfg.RefreshTTL,
+	}
+}
+
+func (s *JWTService) GenerateAccessToken(userID uuid.UUID, role domain.Role) (string, error) {
+	claims := Claims{
+		UserID: userID.String(),
+		Role:   role,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(s.accessTTL)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	signed, err := token.SignedString(s.accessSecret)
+	if err != nil {
+		return "", fmt.Errorf("Sing acces token error: %v", err)
+	}
+
+	return signed, nil
+}
+
+func (s *JWTService) GenerateRefreshToken() (token string, expiresAt time.Time, err error) {
+	expiresAt = time.Now().Add(s.refreshTTL)
+
+	claims := jwt.RegisteredClaims{
+		ExpiresAt: jwt.NewNumericDate(expiresAt),
+		IssuedAt:  jwt.NewNumericDate(time.Now()),
+	}
+
+	t := jwt.NewWithClaims(jwt.SigningMethodES256, claims)
+	token, err = t.SignedString(s.refreshSecret)
+	if err != nil {
+		return "", time.Time{}, fmt.Errorf("Sign refresh token error: %v", err)
+	}
+
+	return token, expiresAt, nil
+}
+
+func (s *JWTService) ValidateAccessToken(tokenStr string) (*Claims, error) {
+	token, err := jwt.ParseWithClaims(tokenStr, &Claims{}, func(t *jwt.Token) (any, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", t.Header["alg"])
+		}
+		return s.accessSecret, nil
+	})
+
+	if err != nil {
+		if errors.Is(err, jwt.ErrTokenExpired) {
+			return nil, domain.ErrExpiredToken
+		}
+		return nil, domain.ErrInvalidToken
+	}
+
+	claims, ok := token.Claims.(*Claims)
+	if !ok || !token.Valid {
+		return nil, domain.ErrInvalidToken
+	}
+
+	return claims, nil
+}
+
+func (s *JWTService) ValidateRefreshToken(tokenStr string) error {
+	_, err := jwt.Parse(tokenStr, func(t *jwt.Token) (any, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signin method")
+		}
+		return s.refreshSecret, nil
+	})
+
+	if err != nil {
+		if errors.Is(err, jwt.ErrTokenExpired) {
+			return domain.ErrExpiredToken
+		}
+		return domain.ErrInvalidToken
+	}
+
+	return nil
+}
