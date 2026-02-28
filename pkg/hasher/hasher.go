fdd9b3e848c851aeb63dddd409c883a8e51a80d2
alikurb12 <gurbansahedovali@gmail.com>
2026-02-28 15:31:13 +0300

    add password hashing with bcrypt and token hashing with SHA-256

--- pkg/hasher/hasher.go
+++ pkg/hasher/hasher.go
@@ -1,1 +1,27 @@
 package hasher
+
+import (
+	"crypto/sha256"
+	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+const bcryptCost = 12
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcryptCost)
+	if err != nil {
+		return "", fmt.Errorf("Hashing password error: %v", err)
+	}
+	return string(bytes), nil
+}
+
+func CheckPassword(password, hash string) bool {
+	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(password)) == nil
+}
+
+func HashToken(token string) string {
+	h := sha256.Sum256([]byte(token))
+	return fmt.Sprintf("%x", h)
+}
